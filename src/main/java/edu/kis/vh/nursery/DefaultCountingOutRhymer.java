3d5758fb31bd436912bf7238865ad1a0759a73ee
OlenaG8 <gaponenkoolena01@gmail.com>
2026-03-16 11:39:23 +0100

    6. Zastąpienie magic numbers deklaracjami stałych w klasie DefaultCountingOutRhymer

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,14 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int STACK_SIZE = 12;
+    public static final int STACK_EMPTY = -1;
+    public static final int STACK_FULL = 11;
+    public static final int ERR_CODE = -1;
 
-    public int total = -1;
+    private int[] numbers = new int[STACK_SIZE];
+
+    public int total = STACK_EMPTY;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +17,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == ERR_CODE;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == STACK_FULL;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return ERR_CODE;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return ERR_CODE;
         return numbers[total--];
     }
 
