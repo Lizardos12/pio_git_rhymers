3569e8f825bdaea6fd102d80304b90feae25fdab
Kacper Nowak <kacper.nowak12112@gmail.com>
2026-03-29 11:05:57 +0200

    9. Zmiana atrybótów klas tak aby widoczność była możliwie najmniejsza

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,14 +2,14 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    public static final int STACK_SIZE = 12;
-    public static final int STACK_EMPTY = -1;
-    public static final int STACK_FULL = STACK_SIZE - 1;
+    private static final int STACK_SIZE = 12;
+    private static final int STACK_EMPTY = -1;
+    private static final int STACK_FULL = STACK_SIZE - 1;
     public static final int ERR_CODE = -1;
 
     private final int[] numbers = new int[STACK_SIZE];
 
-    public int total = STACK_EMPTY;
+    private int total = STACK_EMPTY;
 
     public void countIn(int in) {
         if (!isFull())
